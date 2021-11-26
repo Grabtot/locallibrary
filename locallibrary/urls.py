58cacbcffc22d88d056809a9709c138c350e4231
Grabtot <dneshotkin@gmail.com>
2021-11-26 14:46:09 +0200

    Создан скелет сайта

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,3 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 """locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +19,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
