fe7872a77618e1be6b705f9807f5edecb09c273d
Grabtot <dneshotkin@gmail.com>
2021-11-29 20:11:16 +0200

    Настроен сайт администрирования

--- catalog/models.py
+++ catalog/models.py
@@ -30,7 +30,14 @@ class Book(models.Model):
         """
         return self.title
 
+    def display_genre(self):
+        """
+        Creates a string for the Genre. This is required to display genre in Admin.
+        """
+        return ', '.join([ genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Genre'
 
+    
     def get_absolute_url(self):
         """
         Returns the url to access a particular book instance.
